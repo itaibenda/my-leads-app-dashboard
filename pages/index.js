18d57b8e6d1571ddafe4d617cc4312e98bbd9a1f
Itai Ben David <itaibd@wix.com>
2024-01-30 14:12:42 +0200

    add manage page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import { WixDesignSystemProvider } from '@wix/design-system';
 
-import Page from './MyPage';
-
 import "@wix/design-system/styles.global.css";
 
 function App() {
   return (
     <WixDesignSystemProvider>
-      <Page/>
     </WixDesignSystemProvider>
   );
 }
---
+++ pages/manage/index.js
@@ -0,0 +1,43 @@
+import { WixDesignSystemProvider } from '@wix/design-system';
+import { Page, Button, Box, Layout, Cell, Card } from '@wix/design-system';
+import "@wix/design-system/styles.global.css";
+
+const Manage = () => <WixDesignSystemProvider>
+<Page height="60vh">
+<Page.Header
+  title="Basic Info"
+  actionsBar={
+    <Box gap="SP2">
+      <Button skin="inverted">Cancel</Button>
+      <Button>Save</Button>
+    </Box>
+  }
+  showBackButton
+  onBackClicked={() => {}}
+/>
+<Page.Content>
+  <Layout>
+    <Cell>
+      <Card>
+        <Card.Header title="Program details" />
+        <Card.Divider />
+        <Card.Content>
+          <Box height="420px" />
+        </Card.Content>
+      </Card>
+    </Cell>
+    <Cell>
+      <Page.Footer divider>
+        <Page.Footer.End>
+          <Box gap="SP2">
+            <Button priority="secondary">Cancel</Button>
+            <Button>Save</Button>
+          </Box>
+        </Page.Footer.End>
+      </Page.Footer>
+    </Cell>
+  </Layout>
+</Page.Content>
+</Page></WixDesignSystemProvider>;
+
+export default Manage;
